e3efa1b6dadb11c1c108d2fb60c4e87a26026056
Blacky372 <frederikabitz@gmail.com>
2014-09-10 19:49:42 +0200

    Migrated Manaboots script to ScriptConfig and fixed most of the bugs. Still doesnt work but it's pretty close.

--- Scripts/Blacky_AutoManaBoots.lua
+++ Scripts/Blacky_AutoManaBoots.lua
@@ -11,12 +11,18 @@
 	
 	Features:
 	- Automatically activate Arcane Boots when its on cooldown and you need the mana
-	
+	- Wait for allies that need mana
+
+	Planned Features:
+	- Adjust the outer distance based on how bad the allies need mana
+	- Drop int and mana items before using Arcanes and pick them up after (if there are no enemies near)
 	
 	Version History:
 	v0.3
 	- Upgraded to the new Ensage version
-	- 
+	- Migrated from HotkeyConfig to ScriptConfig
+	- Optimized inner range
+	- Added a range_display around the hero when there are allies that need to get in range
 	v0.2b
 	- Fixed a problem resulting in an error
 	v0.2a
@@ -40,30 +46,41 @@
 ]]
 
 require("libs.Utils")
-require("libs.HotkeyConfig")
+require("libs.ScriptConfig")  
 
 
-ScriptConfig = ConfigGUI:New(script.name)
+ScriptConfig = ScriptConfig.new()
+
+local me = entityList:GetMyHero()
 
 
-ScriptConfig:SetName("Blacky's Auto Mana Boots")
-ScriptConfig:AddParam("AutoManaBoots", "AutoManaBoots", SGC_TYPE_TOGGLE, false, true, nil)
-ScriptConfig:AddParam("AutoManaBootsManaRequired", "Required Mana", SGC_TYPE_NUMCYCLE, false, 100, nil, 25, 400, 25)
-ScriptConfig:AddParam("AutoManaBootsWaitForAllies", "Wait for Allies", SGC_TYPE_TOGGLE, false, true, nil)
-ScriptConfig:AddParam("AutoManaBootsManaAllies", "Required Mana (Allies)", SGC_TYPE_NUMCYCLE, false, 50, nil, 50, 1000, 50)
+--ScriptConfig:SetName("Blacky's Auto Mana Boots")
+ScriptConfig:SetParameter("Enabled", true)
+ScriptConfig:SetParameter("ManaRequired", 100)
+ScriptConfig:SetParameter("WaitForAllies",true)
+ScriptConfig:SetParameter("ManaAllies", 50)
+ScriptConfig:Load()
 
-iconx        = 10
-icony        = 200
+local F10 = drawMgr:CreateFont("F10","Arial",10,500) --TODO: Decide which font is best and remove others!
+local F11 = drawMgr:CreateFont("F11","Arial",11,500)
+local F12 = drawMgr:CreateFont("F12","Arial",12,500)
+local F13 = drawMgr:CreateFont("F13","Arial",13,500)
+local F14 = drawMgr:CreateFont("F14","Arial",14,500)
 
-text         = drawManager:CreateText(iconx + 55, icony, 0xFFFFFFFF, "")
-icon         = drawManager:CreateRectM(iconx, icony, 40, 25, "NyanUI/items/arcane_boots")
+local iconx        = 280
+local icony        = 5
 
-text.visible = false
-icon.visible = false
+local inDistance   = 580 -- Range of Mana replenish is 600, but I added a 20 range buffer because of possible delay
+local inDistance_shown = inDistance-40 -- this is what the range_display effect on the player will be to avoid misleading and accidentally not getting some heroes in the Mana AOE
+local outDistance  = 1800 -- Maybe reduce to 1200 range, maybe scale range with how bad they need mana (If they are really low on mana the script will wait even if they are further away)
 
-inDistance   = 550
-outDistance  = 1800
+local text         = drawMgr:CreateText(iconx + 55, icony, 0xFFFFFFFF, "", F14)
+local icon         = drawMgr:CreateRect(iconx, icony, 40, 25, 0xCCCCCC, drawMgr:GetTextureId("NyanUI/items/arcane_boots"))
+text.visible = true
+icon.visible = true
 
+local effect_arcaneBootsRange = Effect(me, "range_display")
+effect_arcaneBootsRange:SetVector(1,Vector(inDistance_shown,0,0))
 
 
 
@@ -71,7 +88,7 @@ function NearbyPlayersNeedingMana()
 	local numberOfPlayersSwag = 0
 	local nearbyPlayers = entityList:FindEntities({type=TYPE_HERO, team=me.team, alive = true})
 	for i,v in ipairs(nearbyPlayers) do
-		if GetDistance2D(v,me) > inDistance and GetDistance2D(v,me) < outDistance and v.mana<v.maxMana-ScriptConfig.AutoManaBootsManaAllies then
+		if GetDistance2D(v,me) > inDistance and GetDistance2D(v,me) < outDistance and v.mana<v.maxMana-ScriptConfig:GetParameter("ManaAllies") then
 			numberOfPlayersSwag = numberOfPlayersSwag + 1
 		end
 	end
@@ -93,14 +110,14 @@ function HasManaBoots()
 end
 
 function NeedsMana()
-	return me.mana < me.maxMana-ScriptConfig.AutoManaBootsManaRequired
+	return me.mana < me.maxMana-ScriptConfig:GetParameter("ManaRequired")
 end
 
 function NearFountain()
 
-	if me.x <= 6800 and me.y <= 6300 and me.team == TEAM_RADIANT then 
+	if me.position.x <= 6800 and me.position.y <= 6300 and me.team == TEAM_RADIANT then 
 		return true
-	elseif me.x >= 5600 and me.y >= 5000 and me.team == TEAM_DIRE then
+	elseif me.position.x >= 5600 and me.position.y >= 5000 and me.team == TEAM_DIRE then
 		return true
 	else 
 		return false
@@ -111,7 +128,7 @@ end
 function Tick(tick)
 	if PlayingGame() and me.alive and HasManaBoots() and NeedsMana() and (not me:IsChanneling()) and (not me.invisible) and (not NearFountain()) and SleepCheck("Blacky's Mana Boots") then
 		
-		if NearbyPlayersNeedingMana() == 0 or (not ScriptConfig.AutoManaBootsWaitForAllies) then
+		if NearbyPlayersNeedingMana() == 0 or (not ScriptConfig:GetParameter("WaitForAllies")) then
 			me:SafeCastItem("item_arcane_boots")
 			Sleep(500,"Blacky's Mana Boots")
 		end
@@ -121,18 +138,18 @@ function Tick(tick)
 end
 
 function Frame()  --TODO: Add a range_display with the range of the mana boots replenish buff, when there are still allies who need to get in the radius
-	if PlayingGame() and me.alive and HasManaBoots() and NeedsMana() and (NearbyPlayersNeedingMana() > 0 or (not ScriptConfig.AutoManaBootsWaitForAllies)) then
+	if PlayingGame() and me.alive and HasManaBoots() and NeedsMana() and (NearbyPlayersNeedingMana() > 0 or (not ScriptConfig:GetParameter("WaitForAllies"))) then
 
 		icon.visible = true
 		text.visible = true
 		text:SetText(tostring(NearbyPlayersNeedingMana()))
+		effect_arcaneBootsRange:SetVector(1,Vector(inDistance_shown,0,0))
 	else
 
 			icon.visible = false
-
-
 			text.visible = false
-
+			effect_arcaneBootsRange:SetVector(0,Vector(inDistance_shown,0,0))
+			
 	end
 end
 
